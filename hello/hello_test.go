333498f1c454ba2c7a534aa1bdf2079558b1ba0e
Ivaylo Novakov <inovakov@gmail.com>
2017-05-23 21:45:44 +0200

    Moar testing!

---
+++ hello/hello_test.go
@@ -0,0 +1,20 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	tests := []struct {
+		input    string // we won't use this one this time but it's good to know how to do it
+		expected string
+	}{
+		{"", "Hello Go!"},
+	}
+
+	for _, test := range tests {
+		actual := hello()
+		if actual != test.expected {
+			t.Errorf("Failed! Expected %s, got %s.", test.expected, actual)
+		}
+	}
+}
