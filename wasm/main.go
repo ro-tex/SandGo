3556c2e2a424436f10dbadaa9e3c3a17626db494
Ivaylo Novakov <inovakov@gmail.com>
2018-09-13 19:50:31 +0200

    Create a function (`add`) and register it in the surrounding JS environment.

--- wasm/main.go
+++ wasm/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// see https://godoc.org/syscall/js
+
+func add(in []js.Value) {
+	js.Global().Set("output", js.ValueOf(in[0].Int()+in[1].Int()))
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.NewCallback(add))
+}
+
 func main() {
+	c := make(chan struct{}, 0)
+
 	println("Hello, WebAssembly!")
+	registerCallbacks()
+
+	<-c
 }
