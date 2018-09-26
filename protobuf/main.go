866e87396ebd17f9ea0bd037cd3f345fb5589dd9
Ivaylo Novakov <inovakov@gmail.com>
2018-09-26 15:33:51 +0200

    Define simple readers for notifications from JSON and PB.

--- protobuf/main.go
+++ protobuf/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 
-	"./notifications"
+	"./notif"
+	"./pb"
+	"github.com/golang/protobuf/proto"
 )
 
 /*
@@ -18,6 +23,38 @@ https://developers.google.com/protocol-buffers/docs/reference/go-generated
 https://developers.google.com/protocol-buffers/docs/encoding
 */
 
+func readNotifsPB(filename string) *pb.Notifications {
+	// Read the existing address book.
+	in, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+	notifs := &pb.Notifications{}
+	if err := proto.Unmarshal(in, notifs); err != nil {
+		log.Fatalln("Failed to parse notifications:", err)
+	}
+	return notifs
+}
+
+func readNotifsJSON(filename string) *notif.Notifications {
+	// Read the existing address book.
+	in, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+	notifs := &notif.Notifications{}
+	if err := json.Unmarshal([]byte(in), &notifs); err != nil {
+		log.Fatalln("Failed to parse notifications:", err)
+	}
+	return notifs
+}
+
 func main() {
-	fmt.Println(notifications.NotificationStatus_name[1])
+	fmt.Println(pb.NotificationStatus_name[1])
+
+	// notifs := readNotifsPB("../notifications.pb")
+	// notifs := readNotifsJSON("../notifications.json")
+	//
+	// fmt.Println(notifs)
+
 }
