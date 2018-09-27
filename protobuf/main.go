73b6dc171e0643018230bdd51aad406485c59e8e
Ivaylo Novakov <inovakov@gmail.com>
2018-09-27 15:37:56 +0200

    Add time tracking.

--- protobuf/main.go
+++ protobuf/main.go
@@ -5,60 +5,109 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"time"
 
 	"./pb"
 	"github.com/golang/protobuf/proto"
 )
 
-func readNotifsFromJSON(filename string) *pb.Notifications {
+func readNotifsFromJSON(filename string) (*pb.Notifications, time.Duration) {
+	start := time.Now()
+
 	in, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatalln("Error reading file:", err)
 	}
+
 	notifs := &pb.Notifications{}
-	if err := json.Unmarshal([]byte(in), &notifs); err != nil {
+
+	err = json.Unmarshal([]byte(in), &notifs)
+	if err != nil {
 		log.Fatalln("Failed to parse notifications:", err)
 	}
-	return notifs
+
+	return notifs, time.Since(start)
 }
 
-func readNotifsFromPB(filename string) *pb.Notifications {
+func readNotifsFromPB(filename string) (*pb.Notifications, time.Duration) {
+	start := time.Now()
+
 	in, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatalln("Error reading file:", err)
 	}
+
 	notifs := &pb.Notifications{}
-	if err := proto.Unmarshal(in, notifs); err != nil {
+
+	err = proto.Unmarshal(in, notifs)
+	if err != nil {
 		log.Fatalln("Failed to parse notifications:", err)
 	}
-	return notifs
+
+	return notifs, time.Since(start)
 }
 
-func writeNotifsToJSON(filename string, notifs *pb.Notifications) {
+func writeNotifsToJSON(filename string, notifs *pb.Notifications) time.Duration {
+	start := time.Now()
+
 	j, err := json.Marshal(notifs)
 	if err != nil {
 		fmt.Println("Err:", err)
 	}
+
 	err = ioutil.WriteFile(filename, []byte(string(j)), 0644)
 	if err != nil {
 		log.Fatalln("Error writing file:", err)
 	}
+
+	return time.Since(start)
 }
 
-func writeNotifsToPB(filename string, notifs *pb.Notifications) {
+func writeNotifsToPB(filename string, notifs *pb.Notifications) time.Duration {
+	start := time.Now()
+
 	pb, err := proto.Marshal(notifs)
 	if err != nil {
 		fmt.Println("Failed to encode notifications:", err)
 	}
+
 	err = ioutil.WriteFile(filename, pb, 0644)
 	if err != nil {
 		log.Fatalln("Error writing file:", err)
 	}
+
+	return time.Since(start)
+}
+
+func prt(msg string, dur time.Duration, div int64) {
+	fmt.Println(msg, float64(int64(dur)/div)/1000/1000, "ms")
 }
 
 func main() {
-	// notifs := readNotifsFromPB("notifications.pb")
-	notifs := readNotifsFromJSON("notifications.json")
+	var runtime time.Duration
+	var notifsPB, notifsJSON *pb.Notifications
+	var durations struct {
+		readPB    time.Duration
+		readJSON  time.Duration
+		writePB   time.Duration
+		writeJSON time.Duration
+	}
 
-	writeNotifsToPB("notifications.pb", notifs)
+	var i int64
+	for i = 0; i < 1; i++ {
+		notifsPB, runtime = readNotifsFromPB("notifications.pb")
+		durations.readPB += runtime
+		notifsJSON, runtime = readNotifsFromJSON("notifications.json")
+		durations.readJSON += runtime
+
+		runtime = writeNotifsToPB("out.pb", notifsPB)
+		durations.writePB += runtime
+		runtime = writeNotifsToJSON("out.json", notifsJSON)
+		durations.writeJSON += runtime
+	}
+
+	prt("readJSON\t", durations.readJSON, i)
+	prt("readPB\t\t", durations.readPB, i)
+	prt("writeJSON\t", durations.writeJSON, i)
+	prt("writePB\t\t", durations.writePB, i)
 }
