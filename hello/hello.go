1da0ee351c6fcf1d0920c9a69cb74330c7351c68
Ivaylo Novakov <inovakov@gmail.com>
2019-05-16 11:22:48 +0200

    More on package initialisation.

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,18 @@
 package hello
 
+var Greeting = "Hello Go!" // exported package-level variable
+
+// this is called on package load. it cannot be called from other functions
+func init() {
+	println(" > This is 'hello' package's init func. < ")
+}
+
+// we can have multiple init functions
+func init() {
+	println(" > This is 'hello' package's second init func. < ")
+}
+
 // Hello returns a hello string.
 func Hello() string {
-	return "Hello Go!"
+	return Greeting
 }
