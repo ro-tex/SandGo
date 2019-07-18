ba412b46888c1d4bb1d0f663e78336006fa8e2bd
Ivaylo Novakov <inovakov@gmail.com>
2019-07-18 15:35:55 +0200

    .

--- scratch.go
+++ scratch.go
@@ -1,7 +1,7 @@
 package main
 
-// a := []int32{1,2,3} // array
 // s := make([]string, 3) // slice
+// s := []int32{1,2,3} 		// slice
 
 // m := make(map[int32]int32)
 // m := map[string]int{"foo": 1, "bar": 2}
