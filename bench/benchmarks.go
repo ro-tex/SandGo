fee3a07b73811ac8906b55a276a4512fb23971e3
Ivaylo Novakov <inovakov@gmail.com>
2020-05-28 11:29:15 +0200

    Benchmarking.

--- bench/benchmarks.go
+++ bench/benchmarks.go
@@ -2,27 +2,70 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"math/rand"
+	"net/http"
+	netpprof "net/http/pprof"
 	"os"
 	"runtime/pprof"
+	"sync"
+	"sync/atomic"
 
 	"gitlab.com/NebulousLabs/fastrand"
 )
 
+// fr generates a random number using fastrand
 func fr() uint64 {
 	return fastrand.Uint64n(1e9)
 }
 
+// rr generates a random number using math.rand
 func rr() uint64 {
 	return uint64(rand.Int63n(1e9))
 }
 
+// s makes a single select on a channel
 func s(ch chan struct{}) {
 	select {
 	case <-ch:
 	default:
 	}
+}
+
+func atomicCount() uint64 {
+	var ops uint64
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			for c := 0; c < 1000; c++ {
+				atomic.AddUint64(&ops, 1)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	return ops
+}
+
+func mutexCount() uint64 {
+	var ops uint64
+	var opsMu sync.Mutex
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			for c := 0; c < 1000; c++ {
+				opsMu.Lock()
+				ops += 1
+				opsMu.Unlock()
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	return ops
 }
 
 /* Disable CPU frequency scaling:
@@ -35,6 +78,7 @@ done
 */
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var web = flag.String("web", "", "start a web server at :6060")
 
 func main() {
 	flag.Parse()
@@ -50,5 +94,17 @@ func main() {
 	for i := 0; i < 1e7; i++ {
 		_ = fr()
 		_ = rr()
+	}
+
+	if *web != "" {
+		go func() {
+			fmt.Println(
+				http.ListenAndServe(
+					"localhost:6060",
+					http.HandlerFunc(netpprof.Index),
+				),
+			)
+		}()
+		// go tool pprof http://localhost:6060/debug/pprof/heap
 	}
 }
--- bench/benchmarks_test.go
+++ bench/benchmarks_test.go
@@ -44,6 +44,7 @@ func BenchmarkRR(b *testing.B) {
 	result = n
 }
 
+// BenchmarkS_Default tests the case where the select hits the default case.
 func BenchmarkS_Default(b *testing.B) {
 	ch := make(chan struct{})
 	for i := 0; i < b.N; i++ {
@@ -51,6 +52,7 @@ func BenchmarkS_Default(b *testing.B) {
 	}
 }
 
+// BenchmarkS_ChanMsg tests the case where the select hits a case with a channel message.
 func BenchmarkS_ChanMsg(b *testing.B) {
 	ch := make(chan struct{})
 	go func() {
@@ -60,5 +62,26 @@ func BenchmarkS_ChanMsg(b *testing.B) {
 	}()
 	for i := 0; i < b.N; i++ {
 		s(ch)
+	}
+}
+
+func BenchmarkCounts(b *testing.B) {
+	tests := []struct {
+		name   string
+		fn     func() uint64
+		result uint64
+	}{
+		{"atomic", atomicCount, 50000},
+		{"mutex", mutexCount, 50000},
+	}
+
+	for _, tt := range tests {
+		var n uint64
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				n = tt.fn()
+			}
+		})
+		result = n
 	}
 }
